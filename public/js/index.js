2848fef79f1f42f50e2eaee88bdca6cbe6af0037
Cameron Jenner <cjenner37@gmail.com>
2017-07-03 15:38:44 -0600

    searchbar returns results

--- public/js/index.js
+++ public/js/index.js
@@ -6,4 +6,19 @@ $(document).ready(function () {
     $('#submit-edit-profile').click(function () {
     	$('#edit-profile-form').submit();
     });
+
+    $('#user-search').keyup(function () {
+    	let searchTerm = $(this).val();
+    	if (searchTerm != "") {
+    		searchFor(searchTerm);
+    	} else {
+    		$('#search-results').empty();
+    	}
+    });
 });
+
+function searchFor(searchTerm) {
+	$.post('/search_users', {name: searchTerm}, function (html) {
+		$('#search-results').html(html);
+	});
+};
